1b3be795fc6ca27e19566de53cb7db410684c74b
JonD45 <jonathan.dohrman@student.swic.edu>
2025-11-03 13:08:58 -0800

    Add assignment 1

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
 // Variables - same as console JavaScript
 let clickCount = 0;
-
+let clickCount2 = 0;
 // Get references to HTML elements
 const greetingElement = document.getElementById("greeting");
 const button = document.getElementById("change-greeting");
 const button2 = document.getElementById("reset-clicks");
+const button3 = document.getElementById("increment-clicks");
+const button4 = document.getElementById("reset-clicks2");
+const button5 = document.getElementById("decrement-clicks");
 const countElement = document.getElementById("click-count");
-
+const countElement2 = document.getElementById("click-count2");
+const backgroundElement = document.body;
 // Function - same logic as console JavaScript
 function updateGreeting() {
   clickCount++;
@@ -17,14 +21,38 @@ function updateGreeting() {
   }
 }
 
+function incrementClicks() {
+  clickCount2++;
+  countElement2.textContent = `Button clicks: ${clickCount2}`;
+  if (clickCount2 >= 10) {
+    backgroundElement.style.backgroundColor = "yellow";
+  }
+}
+
+function decrementClicks() {
+  clickCount2--;
+  countElement2.textContent = `Button clicks: ${clickCount2}`;
+  if (clickCount2 < 10) {
+    backgroundElement.style.backgroundColor = "red";
+  }
+}
+
 function resetClicks() {
   clickCount = 0;
   countElement.textContent = `Button clicks: ${clickCount}`;
 }
 
+function resetClicks2() {
+  clickCount2 = 0;
+  countElement2.textContent = `Button clicks: ${clickCount2}`;
+  backgroundElement.style.backgroundColor = "white";
+}
+
 button.addEventListener("click", updateGreeting);
 button2.addEventListener("click", resetClicks);
-
+button3.addEventListener("click", incrementClicks);
+button4.addEventListener("click", resetClicks2);
+button5.addEventListener("click", decrementClicks);
 const customGreetingInput = document.getElementById("custom-greeting");
 const setGreetingButton = document.getElementById("set-greeting");
 
